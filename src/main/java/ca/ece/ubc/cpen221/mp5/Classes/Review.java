90f8cc85cf254605d7035d7a1fee357264710e2e
anas desouky <anasdesouky@anass-MacBook-Pro.local>
2017-11-28 23:01:30 -0800

    finished step1

--- src/main/java/ca/ece/ubc/cpen221/mp5/Classes/Review.java
+++ src/main/java/ca/ece/ubc/cpen221/mp5/Classes/Review.java
@@ -1,5 +1,70 @@
 package ca.ece.ubc.cpen221.mp5.Classes;
 
-public class Review {
+import java.util.*;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class Review {
+	private String text;
+	private String date;
+	private String review_id;
+
+
+	public Review(String s) {
+		
+		JSONParser parser = new JSONParser();
+		
+		Scanner Sc= new Scanner(s);
+	while(Sc.hasNextLine()) {
+		
+			Object obj;
+			try {
+				obj = parser.parse(Sc.nextLine());
+				JSONObject jsonObject = (JSONObject) obj;
+				review_id= (String) jsonObject.get("review_id");
+				text= (String) jsonObject.get("text");
+				date= (String) jsonObject.get("date");
+				
+				
+			}
+			catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
+	Sc.close();
+		
+		
+		
+		
+	}
+	
+	public String getText() {
+		return new String(text);
+	}
+	
+	
+	public void setText(String s) {
+		this.text=s;
+	}
+	
+	public String getReview_id() {
+		return new String(review_id);
+	}
+	
+	public void setReview_id(String s) {
+		review_id=s;
+	}
+	
+	public String getDate() {
+		return new String(date);
+	}
+	
+	public void setDate(String s) {
+		this.date=s;
+	}
+	
+	
 }
--- src/main/java/ca/ece/ubc/cpen221/mp5/Classes/YelpUser.java
+++ src/main/java/ca/ece/ubc/cpen221/mp5/Classes/YelpUser.java
@@ -1,5 +1,74 @@
 package ca.ece.ubc.cpen221.mp5.Classes;
 
-public class YelpUser {
+import java.util.*;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class YelpUser extends User{
+	private Map<String, Long> votes= new HashMap<>();
+	private Long review_count;
+	private String type;
+	private double average_stars;
+	
+	public YelpUser(String s) {
+		super(s);
+		JSONParser parser = new JSONParser();
+		
+		Scanner Sc= new Scanner(s);
+	while(Sc.hasNextLine()) {
+		
+			Object obj;
+			try {
+				obj = parser.parse(Sc.nextLine());
+				JSONObject jsonObject = (JSONObject) obj;
+				
+				votes = (Map<String, Long>) jsonObject.get("votes");
+				review_count = (Long) jsonObject.get(review_count);
+				type = (String) jsonObject.get("type");
+				average_stars = (double) jsonObject.get("average_stars");
+				
+				
+				
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+	}
+	Sc.close();
+}
+	
+	
+	public Map<String, Long> getVotes(){
+		return new HashMap<String, Long>( votes);
+	}
+	
+	public void addVote(String s) {
+		votes.replace(s, votes.get(s)+1);
+	}
+	public void removeVote(String s) {
+		votes.replace(s, votes.get(s)-1);
+	}
+	
+	
+	public Long getReview_count() {
+		return new Long(review_count);
+	}
+	public void setReview_count(Long n) {
+		review_count=n;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type=type;
+	}
+	public double getAverage_stars() {
+		return new Double(average_stars);
+	}
+	public void setAverage_stars(double n) {
+		average_stars=n;
+	}
 }
