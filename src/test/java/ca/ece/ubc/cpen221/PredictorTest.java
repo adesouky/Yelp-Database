a7078d60b450f5d9a74c0084013719dc333f7b02
adesouky <31939557+adesouky@users.noreply.github.com>
2017-12-03 02:03:16 -0800

    Changed travis build path for tests

---
+++ src/test/java/ca/ece/ubc/cpen221/PredictorTest.java
@@ -0,0 +1,68 @@
+package ca.ece.ubc.cpen221;
+
+import static org.junit.Assert.*;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.ToDoubleBiFunction;
+
+import org.junit.Test;
+
+import ca.ece.ubc.cpen221.mp5.Centroid;
+import ca.ece.ubc.cpen221.mp5.MP5Db;
+import ca.ece.ubc.cpen221.mp5.Classes.*;
+
+public class PredictorTest {
+
+	
+	@Test
+	public void Test1() throws FileNotFoundException {
+		Database x= new Database("data/restaurants.json", "data/reviews.json" , "data/users.json");
+		ToDoubleBiFunction<MP5Db<Restaurant>, String> y =x.getPredictorFunction("Y5j_2YR42UHvWZOtALgjCw");
+		Double z= y.applyAsDouble((Database) x, "6QZR4ToHKlse0yhqpU5ijg");
+		System.out.println(z);
+	}
+	
+	@Test
+	public void Test2() throws FileNotFoundException {
+		Database x= new Database("data/restaurants.json", "data/reviews.json" , "data/users.json");
+		int k=25;
+		System.out.println(x.kMeansClusters_json(k));
+		Map<Centroid, Set<Restaurant>> restaurantMap = x.kMeansClusters_Map(k);
+		//System.out.println(restaurantMap.size());
+		List<Centroid> CentroidList = new ArrayList<>(restaurantMap.keySet());
+		
+
+		
+		for(int i=0; i<k ; i++) {
+		Set<Restaurant> current = restaurantMap.get(CentroidList.get(i));
+		for(Restaurant rest : current) {
+			Centroid currentCent = CentroidList.get(0);
+			double minDistance = Double.MAX_VALUE;
+			for(Centroid cent : CentroidList) {
+				double distance = cent.findDistance(rest.getLatitude(), rest.getLongitude());
+				
+				//minDistance = Math.min(distance, minDistance);
+				if(distance < minDistance) {
+					minDistance = distance;
+					currentCent = cent; 
+				}
+			}
+			if(CentroidList.indexOf(currentCent) != i) {
+				System.out.println(CentroidList.indexOf(currentCent));
+				System.out.println(CentroidList.get(11).findDistance(rest.getLatitude(), rest.getLongitude()));
+				System.out.println(CentroidList.get(4).findDistance(rest.getLatitude(), rest.getLongitude()));
+
+				fail("Should Equal" + i + "but was" + CentroidList.indexOf(currentCent));
+			}
+		}
+		}
+		
+	}
+	
+	
+	
+}
---
+++ src/test/java/ca/ece/ubc/cpen221/UserTest.java
@@ -0,0 +1,45 @@
+package ca.ece.ubc.cpen221;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import ca.ece.ubc.cpen221.mp5.Classes.User;
+
+
+public class UserTest {
+
+	@Test
+	public void test01() {
+//		Database db = new Database("data/restaurants.json", "data/reviews.json" , "data/users.json");
+			User user1 = new User("megan");
+			
+			user1.setUrl("url1");
+			assertEquals("url1", user1.getUrl());
+			
+		//	db.addRestaurant(rest1);
+		}
+	
+	@Test
+	public void test02() {
+//		Database db = new Database("data/restaurants.json", "data/reviews.json" , "data/users.json");
+			User user1 = new User("megan");
+			
+			user1.setName("Name1");
+			assertEquals("Name1", user1.getName());
+			
+		//	db.addRestaurant(rest1);
+		}
+	
+	@Test
+	public void test03() {
+//		Database db = new Database("data/restaurants.json", "data/reviews.json" , "data/users.json");
+			User user1 = new User("megan");
+			
+			user1.setUser_id("UserID1");
+			assertEquals("UserID1", user1.getUser_id());
+			
+		//	db.addRestaurant(rest1);
+		}
+
+
+}
